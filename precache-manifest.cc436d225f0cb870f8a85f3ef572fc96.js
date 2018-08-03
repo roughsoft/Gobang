5e924d2403f3e3c6a2e9491f5e65916cb5fd5730
陈水超 <22988248@qq.com>
2018-08-03 14:46:10 +0800

    init

---
+++ precache-manifest.cc436d225f0cb870f8a85f3ef572fc96.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ba6457e28b5c8a1625be50381b28fbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffff4377511f62df974614b099ad3f67",
+    "url": "/img/sad.gif"
+  },
+  {
+    "revision": "ffff4377511f62df974614b099ad3f67",
+    "url": "/img/sad.ffff4377.gif"
+  },
+  {
+    "revision": "9ad347b59b92ffb056ef878804944200",
+    "url": "/img/haha.gif"
+  },
+  {
+    "revision": "9ad347b59b92ffb056ef878804944200",
+    "url": "/img/haha.9ad347b5.gif"
+  },
+  {
+    "revision": "5442f90f0a7d84ccc30d008200e3aee6",
+    "url": "/img/board.5442f90f.jpg"
+  },
+  {
+    "revision": "56a5da18d0de7d161e18",
+    "url": "/css/app.6e190c93.css"
+  },
+  {
+    "revision": "56a5da18d0de7d161e18",
+    "url": "/app.bundle.js"
+  },
+  {
+    "revision": "2f8e13ee68c69aaaa538",
+    "url": "/ai.bundle.js"
+  }
+];
